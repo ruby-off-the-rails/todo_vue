2ea02aaa632e621e7e5d4e41dad8e1b21d5d5f49
brian <riceb53@gmail.com>
2019-12-23 19:17:18 -0600

    display tasks using v-for

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "byebye Vue!",
+    tasks: [
+      {text: "Trash", completed: false},
+      {text: "Bed", completed: false},
+      {text: "Read", completed: false},
+    ],
+  },
+  methods: {
+
+  }
+});
