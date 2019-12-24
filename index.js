e610517c2416d070676c31562394099997d1c620
brian <riceb53@gmail.com>
2019-12-23 20:11:00 -0600

    add and delete tasks

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ var app = new Vue({
     tasks: [
       {text: "Trash", completed: false},
       {text: "Bed", completed: false},
-      {text: "Read", completed: false},
+      {text: "Read", completed: false}
     ],
+    newTask: ""
   },
   methods: {
+    addTask: function() {
+      console.log('add task');
+      // add newTask into the array (tasks)
 
+      this.tasks.push({ text: this.newTask, completed: false});
+      console.log("this.newTask")
+      console.log(this.newTask)
+      console.log("this.tasks")
+      console.log(this.tasks)
+    },
+    deleteTask: function(theTask) {
+      console.log('deleteTask')
+      console.log(theTask)
+      var index = this.tasks.indexOf(theTask);
+      console.log(index);
+      this.tasks.splice(index, 1);
+    }
   }
 });
