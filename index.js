1b81350a90319ad18b9f993baf0aafae20a17c42
brian <riceb53@gmail.com>
2019-12-23 20:34:06 -0600

    filter based on complete/incomplete

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var app = new Vue({
     message: "byebye Vue!",
     tasks: [
       {text: "Trash", completed: false},
-      {text: "Bed", completed: false},
+      {text: "Bed", completed: true},
       {text: "Read", completed: false}
     ],
-    newTask: ""
+    newTask: "",
+    taskFilter: 'incomplete'
   },
   methods: {
     addTask: function() {
@@ -26,6 +27,41 @@ var app = new Vue({
       var index = this.tasks.indexOf(theTask);
       console.log(index);
       this.tasks.splice(index, 1);
+    }
+  },
+  computed: {
+    incompleteTasks: function() {
+      // find all the tasks that aren't done yet
+      var sum = 0;
+      this.tasks.forEach(function(task) {
+        if (task.completed === false) {
+          sum++;
+        }
+      })
+      return sum;
+    },
+    shownTasks: function() {
+      // show all the tasks
+      if (this.taskFilter === 'all') {
+        return this.tasks;
+      } else if (this.taskFilter === 'complete') {
+        var newArray = [];
+        this.tasks.forEach(function(task) {
+          if (task.completed === true) {
+            newArray.push(task);
+          }
+        })
+        return newArray;
+      } else if (this.taskFilter === 'incomplete') {
+              // show all completed
+        var newArray = [];
+        this.tasks.forEach(function(task) {
+          if (task.completed === false) {
+            newArray.push(task);
+          }
+        })
+        return newArray;
+      }
     }
   }
 });
